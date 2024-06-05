7968d258b31b14022545d54003fabee3bab34dcd
noirety <noirety@github.com>
2024-06-05 15:17:27 +0800

    单位转换为GB

--- shell/query_xray_traffic.sh
+++ shell/query_xray_traffic.sh
@@ -6,24 +6,31 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# 函数：查询流量并转换为 MB
+# 函数：查询流量并转换为适当的单位
 query_traffic() {
-  local temp_name=$1
+  local name=$1
   local direction=$2
-  local result=$(xray api stats --server=127.0.0.1:31399 -name "inbound>>>$temp_name>>>traffic>>>$direction")
+  local result=$(xray api stats --server=127.0.0.1:31399 -name "inbound>>>$name>>>traffic>>>$direction")
   local value=$(echo $result | jq -r '.stat.value')
   local value_in_mb=$(echo "scale=2; $value / (1024 * 1024)" | bc)
-  echo $value_in_mb
+
+  if (( $(echo "$value_in_mb >= 1024" | bc -l) )); then
+    local value_in_gb=$(echo "scale=2; $value_in_mb / 1024" | bc)
+    echo "$value_in_gb GB"
+  else
+    echo "$value_in_mb MB"
+  fi
 }
 
 # 遍历所有传入的参数（用户名）
 for name in "$@"; do
   # 查询下载量和上传量
-  down_value_in_mb=$(query_traffic "$name" "downlink")
-  up_value_in_mb=$(query_traffic "$name" "uplink")
+  down_value=$(query_traffic "$name" "downlink")
+  up_value=$(query_traffic "$name" "uplink")
+
   # 输出结果
-  echo "用户: $name"
-  echo "下载量: $down_value_in_mb MB"
-  echo "上传量: $up_value_in_mb MB"
+  echo "$name:"
+  echo "下载量：$down_value"
+  echo "上传量：$up_value"
   echo "---------------------"
 done
